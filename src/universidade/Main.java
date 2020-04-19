4532c26ce212884db13887f36f1a41d6ab5c4214
Wilson Felipe <wilsonfelipe15@gmail.com>
2020-04-19 20:47:20 -0300

    object name change

--- src/universidade/Main.java
+++ src/universidade/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	
 	public static void main(String args[]) {
 		
-		Database db2 = new Database();
+		Database db = new Database();
 		
 		/*
 		 * Exemplos de como utilizar os m�todos da classe Database, para acessar informa��es do banco de dados
@@ -15,31 +15,31 @@ public class Main {
 		 */ 
 		
 		System.out.println("------ Pesquisando Aluno com ID igual a 100 ------");
-		db2.searchAlunoById("100").imprimirDados();
+		db.searchAlunoById("100").imprimirDados();
 		System.out.println("\n------ Pesquisando Aluno com nome igual a Lucas ------");
-		for (Aluno aluno : db2.searchAlunoByNome("João Lucas")) {
+		for (Aluno aluno : db.searchAlunoByNome("João Lucas")) {
 			aluno.imprimirDados();
 			System.out.println("*********");	
 		}	
 		
 		System.out.println("\n------ Pesquisando Curso com ID igual a 5 ------");
-		db2.searchCursoById("5").imprimirDados();
+		db.searchCursoById("5").imprimirDados();
 		System.out.println("\n------ Pesquisando Curso com nome igual a Ciência ------");
-		for (Curso curso : db2.searchCursoByNome("Ciência")) {
+		for (Curso curso : db.searchCursoByNome("Ciência")) {
 			curso.imprimirDados();
 			System.out.println("*********");	
 		}		
 		
 		System.out.println("\n------ Pesquisando Disicplina com ID igual a 5 ------");
-		db2.searchDisciplinaById("66").imprimirDados();
+		db.searchDisciplinaById("66").imprimirDados();
 		System.out.println("\n------ Pesquisando Disicplina com nome igual a Geoprocessamento ------");
-		for (Disciplina disciplina : db2.searchDisciplinaByNome("Produção Vegetal II")) {
+		for (Disciplina disciplina : db.searchDisciplinaByNome("Produção Vegetal II")) {
 			disciplina.imprimirDados();
 			System.out.println("*********");	
 		}	
 
 		System.out.println("\n------ Pesquisando Nota com ID igual a 365 ------");
-		db2.searchNotaById("365").imprimirDados();
+		db.searchNotaById("365").imprimirDados();
 		
 		/*
 		System.out.println("\n---------- Inserindo um aluno no BD ----------");
